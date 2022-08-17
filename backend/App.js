363363e50bf66fadaf074ba654d3217113c6cf41
Andrey Mirenkov <Andreymirenkov@yandex.ru>
2022-08-17 16:20:11 +0300

    Проектная работа 15(server)

--- backend/App.js
+++ backend/App.js
@@ -29,13 +29,12 @@ app.use((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials: true');
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials: true');
     return res.end();
   }
   next();
